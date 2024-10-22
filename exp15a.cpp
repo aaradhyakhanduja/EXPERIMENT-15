9b0e2029d8fadd29a6579305c0e9f2fd4b4491d5
aaradhyakhanduja <k.aaradhya1664@gmail.com>
2024-10-22 13:55:31 +0530

    Update exp15a.cpp

--- exp15a.cpp
+++ exp15a.cpp
@@ -1,2 +1,25 @@
 //aaradhya khanduja
 //23070123156
+
+//factorial
+#include <iostream>
+using namespace std;
+int fact(int n)
+{
+    if (n<=1)
+    {
+        return 1;
+    }
+    else
+    {
+        return (n*fact(n-1));
+    }
+}
+int main()
+{
+    int x,n;
+    cout<<"Enter a number: ";
+    cin>>n;
+    x= fact(n);
+    cout<<n<<"!: "<<x;
+}
