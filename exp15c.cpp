35eeb7393e1f72863f1bf853deb9beb9f6c25da9
aaradhyakhanduja <k.aaradhya1664@gmail.com>
2024-10-22 13:54:31 +0530

    Update exp15c.cpp

--- exp15c.cpp
+++ exp15c.cpp
@@ -1,1 +1,23 @@
+//aaradhya khanduja
+//23070123156
 
+#include <iostream>
+#include <string>
+using namespace std;
+void rev(char *str)
+{
+    if (*str)
+    {
+        rev(str+1);
+        cout<<("%c",*str);
+
+    }
+
+}
+int main()
+{
+    char s[50];
+    cout<<"Enter a string: ";
+    cin>>s;
+    rev(s);
+}
