fb73b0e1b5541cd68d57a00fd5120e6940f4dbbf
aaradhyakhanduja <k.aaradhya1664@gmail.com>
2024-10-22 13:56:09 +0530

    Update exp15d.cpp

--- exp15d.cpp
+++ exp15d.cpp
@@ -1,2 +1,24 @@
 //aaradhya khanduja
 //23070123156
+
+#include <iostream>
+#include <string>
+using namespace std;
+void revn(int i)
+{
+    if (i>0)
+    {
+        cout<<i%10;
+        revn(i/10);
+        
+
+    }
+
+}
+int main()
+{
+    int i;
+    cout<<"Enter a number: ";
+    cin>>i;
+    revn(i);
+}
